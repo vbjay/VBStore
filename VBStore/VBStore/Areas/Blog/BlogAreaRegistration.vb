729cb2855414d542627fd517c1ef4794699b185c
Jay Asbury <vbjay.net@gmail.com>
2017-06-18 19:58:24 -0400

    Add Blog area

---
+++ VBStore/VBStore/Areas/Blog/BlogAreaRegistration.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Web.Mvc
+
+Namespace Areas.Blog
+    Public Class BlogAreaRegistration
+        Inherits AreaRegistration
+
+        Public Overrides ReadOnly Property AreaName() As String
+            Get
+                Return "Blog"
+            End Get
+        End Property
+
+        Public Overrides Sub RegisterArea(ByVal context As AreaRegistrationContext)
+            context.MapRoute(
+                "Blog_default",
+                "Blog/{controller}/{action}/{id}",
+                New With {.action = "Index", .id = UrlParameter.Optional, .controller = "Home"}
+            )
+        End Sub
+    End Class
+End Namespace
